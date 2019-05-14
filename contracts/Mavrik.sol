b563d830b883ffb17032052c2bcb39e83e4dd3f4
Adi Kancherla <findadit@gmail.com>
2019-05-14 14:51:59 +0200

    minor

---
+++ contracts/Mavrik.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+import "./ERC1155MixedFungibleMintableBurnable.sol";
+
+/**
+    @dev Mintable form of ERC1155
+    Shows how easy it is to mint new items
+*/
+contract Mavrik is ERC1155MixedFungibleMintableBurnable {
+	
+}
