582303db7f0b2af546d08964d84605164ef45c03
Adi Kancherla <findadit@gmail.com>
2019-05-15 20:15:04 +0200

    added zos

--- truffle-config.js
+++ truffle-config.js
@@ -7,17 +7,12 @@ module.exports = {
   // for more details on how to specify configuration options!
   contracts_build_directory: path.join(__dirname, "build"),
   networks: {
-    ganachecli: {
+    local: {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*"
     },
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
-    },
-    test: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*"
