85aafb415d78a22f47fb1801bff19feef6b5d177
Adi Kancherla <findadit@gmail.com>
2019-05-21 08:50:48 -0700

    name change and changes

--- contracts/MavrikMultiNFT.sol
+++ contracts/MavrikMultiNFT.sol
@@ -185,7 +185,7 @@ contract PauserRole is Initializable {
 
     Roles.Role private _pausers;
 
-    function initialize(address sender) public initializer {
+    function _initialize(address sender) internal initializer {
         if (!isPauser(sender)) {
             _addPauser(sender);
         }
@@ -228,8 +228,8 @@ contract Pausable is Initializable, PauserRole {
 
     bool private _paused;
 
-    function initialize(address sender) public initializer {
-        PauserRole.initialize(sender);
+    function _initialize(address sender) internal initializer {
+        PauserRole._initialize(sender);
 
         _paused = false;
     }
@@ -337,6 +337,12 @@ contract IERC721Enumerable is Initializable, IERC721 {
     function tokenByIndex(uint256 index) public view returns (uint256);
 }
 
+contract IERC721Metadata is Initializable, IERC721 {
+    function name() external view returns (string memory);
+    function symbol() external view returns (string memory);
+    function tokenURI(uint256 tokenId) external view returns (string memory);
+}
+
 contract ERC165 is Initializable, IERC165 {
     bytes4 private constant _INTERFACE_ID_ERC165 = 0x01ffc9a7;
     /**
@@ -353,7 +359,7 @@ contract ERC165 is Initializable, IERC165 {
      * @dev A contract implementing SupportsInterfaceWithLookup
      * implement ERC165 itself
      */
-    function initialize() public initializer {
+    function _initialize() internal initializer {
         _registerInterface(_INTERFACE_ID_ERC165);
     }
 
@@ -373,12 +379,6 @@ contract ERC165 is Initializable, IERC165 {
     }
 
     uint256[50] private ______gap;
-}
-
-contract IERC721Metadata is Initializable, IERC721 {
-    function name() external view returns (string memory);
-    function symbol() external view returns (string memory);
-    function tokenURI(uint256 tokenId) external view returns (string memory);
 }
 
 contract ERC721 is Initializable, ERC165, IERC721 {
@@ -415,8 +415,8 @@ contract ERC721 is Initializable, ERC165, IERC721 {
      *     bytes4(keccak256('safeTransferFrom(address,address,uint256,bytes)'))
      */
 
-    function initialize() public initializer {
-        ERC165.initialize();
+    function _initialize() internal initializer {
+        ERC165._initialize();
 
         // register the supported interfaces to conform to ERC721 via ERC165
         _registerInterface(_INTERFACE_ID_ERC721);
@@ -664,9 +664,9 @@ contract ERC721 is Initializable, ERC165, IERC721 {
 }
 
 contract ERC721Pausable is Initializable, ERC721, Pausable {
-    function initialize(address sender) public initializer {
+    function _initialize(address sender) internal initializer {
         require(ERC721._hasBeenInitialized());
-        Pausable.initialize(sender);
+        Pausable._initialize(sender);
     }
 
     function approve(address to, uint256 tokenId) public whenNotPaused {
@@ -705,7 +705,7 @@ contract ERC721Metadata is Initializable, ERC165, ERC721, IERC721Metadata {
     /**
      * @dev Constructor function
      */
-    function initialize(string memory name, string memory symbol) public initializer {
+    function _initialize(string memory name, string memory symbol) internal initializer {
         require(ERC721._hasBeenInitialized());
 
         _name = name;
@@ -715,9 +715,9 @@ contract ERC721Metadata is Initializable, ERC165, ERC721, IERC721Metadata {
         _registerInterface(_INTERFACE_ID_ERC721_METADATA);
     }
 
-  function _hasBeenInitialized() internal view returns (bool) {
-    return supportsInterface(_INTERFACE_ID_ERC721_METADATA);
-}
+    function _hasBeenInitialized() internal view returns (bool) {
+        return supportsInterface(_INTERFACE_ID_ERC721_METADATA);
+    }
     /**
      * @dev Gets the token name
      * @return string representing the token name
@@ -752,6 +752,7 @@ contract ERC721Metadata is Initializable, ERC165, ERC721, IERC721Metadata {
      */
     function _setTokenURI(uint256 tokenId, string memory uri) internal {
         require(_exists(tokenId));
+        require(bytes(_tokenURIs[tokenId]).length != 0);
         _tokenURIs[tokenId] = uri;
     }
 
@@ -769,6 +770,54 @@ contract ERC721Metadata is Initializable, ERC165, ERC721, IERC721Metadata {
         if (bytes(_tokenURIs[tokenId]).length != 0) {
             delete _tokenURIs[tokenId];
         }
+    }
+
+    uint256[50] private ______gap;
+}
+
+contract ERC721MultiMetadata is ERC721Metadata {
+
+    uint256 constant _TYPE_MASK = uint256(uint128(~0)) << 128;
+    // Token type to name mapping
+    mapping(uint256 => string) private _names;
+
+    // Token type to symbol mapping
+    mapping(uint256 => string) private _symbols;
+
+    /**
+     * @dev Gets the token name of the given tokenId
+     * @return string representing the token name
+     */
+    function name(uint256 tokenId) external view returns (string memory) {
+        uint256 tokenType = tokenId & _TYPE_MASK;
+        return _names[tokenType];
+    }
+
+    /**
+     * @dev Gets the token symbol
+     * @return string representing the token symbol
+     */
+    function symbol(uint256 tokenId) external view returns (string memory) {
+        uint256 tokenType = tokenId & _TYPE_MASK;
+        return _symbols[tokenType];
+    }
+
+    /**
+     * @dev Sets name for the token type
+     * @return string representing the token symbol
+     */
+    function _setName(uint256 tokenType, string memory tokenName) internal {
+        require(bytes(_names[tokenType]).length != 0 && bytes(tokenName).length != 0);
+        _names[tokenType] = tokenName;
+    }
+
+    /**
+     * @dev Sets name for the token type
+     * @return string representing the token symbol
+     */
+    function _setSymbol(uint256 tokenType, string memory tokenSymbol) internal {
+        require(bytes(_symbols[tokenType]).length != 0 && bytes(tokenSymbol).length != 0);
+        _symbols[tokenType] = tokenSymbol;
     }
 
     uint256[50] private ______gap;
@@ -798,7 +847,7 @@ contract ERC721Enumerable is Initializable, ERC165, ERC721, IERC721Enumerable {
     /**
      * @dev Constructor function
      */
-    function initialize() public initializer {
+    function _initialize() internal initializer {
         require(ERC721._hasBeenInitialized());
 
         // register the supported interface to conform to ERC721 via ERC165
@@ -975,7 +1024,7 @@ contract ERC721Enumerable is Initializable, ERC165, ERC721, IERC721Enumerable {
  * @title Multi ERC721 token
  *
  */
-contract MavrikNFTs is Initializable, ERC721, ERC721Enumerable, ERC721Metadata, ERC721Pausable {
+contract MultiNFT is Initializable, ERC721, ERC721Enumerable, ERC721MultiMetadata, ERC721Pausable {
 
     // Use a split bit implementation.
     // Store the type in the upper 128 bits
@@ -1004,12 +1053,12 @@ contract MavrikNFTs is Initializable, ERC721, ERC721Enumerable, ERC721Metadata, 
     }
 
     function initialize(string memory name, string memory symbol, address[] memory pausers) public initializer {
-        ERC721.initialize();
-        ERC721Enumerable.initialize();
-        ERC721Metadata.initialize(name, symbol);
+        ERC721._initialize();
+        ERC721Enumerable._initialize();
+        ERC721Metadata._initialize(name, symbol);
 
         // Initialize the pauser roles, and renounce them
-        ERC721Pausable.initialize(address(this));
+        ERC721Pausable._initialize(address(this));
         _removePauser(address(this));
 
         // Add the requested pausers (this can be done after renouncing since
@@ -1030,7 +1079,7 @@ contract MavrikNFTs is Initializable, ERC721, ERC721Enumerable, ERC721Metadata, 
     }
 
     // This function only creates the type.
-    function createType(string calldata uri) external returns(uint256) {
+    function createType(string calldata name, string calldata symbol, string calldata uri) external whenNotPaused returns(uint256) {
         require(_tokenType < _TYPE_MASK);
         // Store the type in the upper 128 bits
         _tokenType = _tokenType.add(1);
@@ -1040,12 +1089,14 @@ contract MavrikNFTs is Initializable, ERC721, ERC721Enumerable, ERC721Metadata, 
 
         // mint the first token of this type with index 0 and send to type creator
         uint256 tokenId = _tokenType << 128;
+        _setName(_tokenType, name);
+        _setSymbol(_tokenType, symbol);
         _mintWithTokenURI(msg.sender, tokenId, uri);
         _typeBalances[_tokenType][msg.sender] = _typeBalances[_tokenType][msg.sender].add(1);
         return _tokenType;
     }
 
-    function mint(uint128 tokenType, address[] calldata to, string calldata uri) external creatorOnly(tokenType) {
+    function mint(uint128 tokenType, address[] calldata to, string calldata uri) external whenNotPaused creatorOnly(tokenType) {
         require(_maxIndexOfType[tokenType].add(to.length) < _INDEX_MASK);
 
         uint256 index = _maxIndexOfType[tokenType].add(1);
@@ -1066,11 +1117,18 @@ contract MavrikNFTs is Initializable, ERC721, ERC721Enumerable, ERC721Metadata, 
         return true;
     }
 
+    // sets only when eixsting URI is blank
+    function setTokenURI(uint256 tokenId, string calldata uri) external whenNotPaused {
+        require(bytes(uri).length != 0);
+        require(ownerOf(tokenId) == msg.sender);
+        _setTokenURI(tokenId, uri);
+    }
+
     /**
      * @dev Burns a specific ERC721 token.
      * @param tokenId uint256 id of the ERC721 token to be burned.
      */
-    function burn(uint256 tokenId) external {
+    function burn(uint256 tokenId) external whenNotPaused {
         address owner = ownerOf(tokenId);
         uint256 tokenType = getType(tokenId);
         _burn(owner, tokenId);
